efdbfa6d95f269d7d626f3baf8cba3894e13fd42
reElcy <reElcy@yandex.ru>
2025-11-24 00:00:19 +0300

    feat: add migration

---
+++ migrations/000004_add_pull_requests_table.down.sql
@@ -0,0 +1,5 @@
+-- 000004_add_pull_requests_table.down.sql
+
+DROP TABLE IF EXISTS pull_requests;
+
+DROP TYPE IF EXISTS pr_status;
---
+++ migrations/000005_add_pr_reviewers_table.down.sql
@@ -0,0 +1,3 @@
+-- 000005_add_pr_reviewers_table.down.sql
+
+DROP TABLE IF EXISTS pr_reviewers;
