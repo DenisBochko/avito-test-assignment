5327d52b2c40fafa0d9c2671fd8754cbb821bc18
reElcy <reElcy@yandex.ru>
2025-11-23 21:02:50 +0300

    feat: add handler

---
+++ internal/api/http/handler/handler.go
@@ -0,0 +1,43 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	StatusNotAvailable = "NOT_AVAILABLE"
+)
+
+type ResponseWithError struct {
+	Error ResponseError `json:"error"`
+}
+
+type ResponseError struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
+type ResponseWithMessage struct {
+	Status  string `json:"status"`
+	Message string `son:"message"`
+}
+
+func NoMethod(c *gin.Context) {
+	c.JSON(http.StatusMethodNotAllowed, ResponseWithError{
+		Error: ResponseError{
+			Code:    StatusNotAvailable,
+			Message: "method not allowed on this endpoint",
+		},
+	})
+}
+
+func NoRoute(c *gin.Context) {
+	c.JSON(http.StatusNotFound, ResponseWithError{
+		Error: ResponseError{
+			Code:    StatusNotAvailable,
+			Message: "page not found",
+		},
+	})
+}
