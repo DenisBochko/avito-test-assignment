2fff71b7ad888885da3ae27344aa488305f7b89f
reElcy <reElcy@yandex.ru>
2025-11-23 21:03:11 +0300

    feat: add route

---
+++ internal/api/http/route/route.go
@@ -0,0 +1,36 @@
+package route
+
+import (
+	"avito-test-assignment/internal/api/http/handler"
+	"avito-test-assignment/internal/api/http/middleware"
+	"avito-test-assignment/internal/config"
+	"io"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+func SetupRouter(
+	l *zap.Logger,
+	cfg *config.Config,
+	teamHdl *handler.TeamHandler,
+) *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
+	gin.DefaultWriter = io.Discard
+
+	router := gin.Default()
+
+	router.Use(middleware.Logger(l))
+	router.Use(middleware.RequestTimeout(cfg.HTTPServer.Timeout.Request))
+
+	router.HandleMethodNotAllowed = true
+	router.NoMethod(handler.NoMethod)
+	router.NoRoute(handler.NoRoute)
+
+	basePath := router.Group(cfg.BasePath)
+
+	teamGroup := basePath.Group("/team")
+	RegisterTeamRoutes(teamGroup, teamHdl)
+
+	return router
+}
